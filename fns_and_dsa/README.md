5538d757605fcfff5ac0c2130f077340c8b6991b
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-06-29 13:51:11 +0300

    perform operation

---
+++ fns_and_dsa/README.md
@@ -0,0 +1,6 @@
+this directory I created is for practice creating functions in python and data structures like:
+lists
+tuples
+dictionaries
+
+it's a part of alx backend program.
---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            result = num1 + num2
+        case 'subtract':
+            result = num1 - num2
+        case 'multiply':
+            result = num1 * num2
+        case 'divide':
+            if num2 == 0:
+                print("invalid operation")
+            else:
+                result = num1/num2
+    return result
